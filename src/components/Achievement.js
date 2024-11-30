eb9505f0384bd639fa068a7c3d4dc38bb96ef4fd
Manya2418 <manyasahu94@gmail.com>
2024-11-30 09:35:42 +0530

    update

--- src/components/Achievement.js
+++ src/components/Achievement.js
@@ -8,13 +8,13 @@ function Achievement() {
       <div className="divider"></div>
       <p> You can find me on various programming platforms and professional websites such as LinkedIn, GitHub, GeeksforGeeks, and LeetCode.Through consistent effort and dedication, I have solved numerous problems across various platforms, honing my problem-solving skills.</p>
       <div className="about-col" >
-        <div className="img-col" style={{flexBasis: "50%"}}>
-          <img src="./Images/leetprofile.png" alt="About Me" />
+        <div className="img-col" style={{flexBasis: "50%",flexDirection:"column"}}>
+          <img src="./Images/leetprofile.png" alt="" /><br/>
           <a   href="https://leetcode.com/u/Manya123/" class="know-btn">
-          Leetcode Profile<i class="fas fa-arrow-right arrow"></i></a>
+             Leetcode   Profile<i class="fas fa-arrow-right arrow"></i></a>
         </div> 
-        <div className="img-col" style={{flexBasis: "50%"}}>
-          <img src="./Images/gfgprofile.png" alt="About Me" />
+        <div className="img-col" style={{flexBasis: "50%",flexDirection:"column"}}>
+          <img src="./Images/gfgprofile.png" alt="About Me" /><br></br>
           <a   href="https://www.geeksforgeeks.org/user/manyasm4rk/" class="know-btn">
           GeeksforGeeks Profile  <i class="fas fa-arrow-right arrow"></i></a>
         </div>
